86f7d67d42dc138fdd145b62fec74e4f9a9432cb
Jason Lengstorf <jason@lengstorf.com>
2019-12-07 14:20:44 -0800

    fix: add CORS handling

--- packages/create-handler/index.js
+++ packages/create-handler/index.js
@@ -1,4 +1,16 @@
 module.exports = sfxHandler => async event => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: JSON.stringify('ok'),
+    };
+  }
+
   const { user, message, flags, extra } = JSON.parse(event.body);
 
   const response = sfxHandler({ user, message, flags, extra });
@@ -7,7 +19,6 @@ module.exports = sfxHandler => async event => {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST',
     },
     body: JSON.stringify(response),
   };
