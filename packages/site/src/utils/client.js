c2cf2379224bf95edafc9a2fc479c1cd47c56d50
Jason Lengstorf <jason@lengstorf.com>
2020-03-08 19:42:32 -0700

    chore: remove console.log

--- packages/site/src/utils/client.js
+++ packages/site/src/utils/client.js
@@ -5,8 +5,5 @@ import { auth } from './auth';
 export const client = new ApolloClient({
   fetch,
   uri: `https://db.streamblitz.com/v1/graphql`,
-  request: operation => {
-    console.log(auth.authHeaders());
-    return operation.setContext({ headers: auth.authHeaders() });
-  },
+  request: operation => operation.setContext({ headers: auth.authHeaders() }),
 });
