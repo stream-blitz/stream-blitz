a212ecde01c73301a6d0259b19c1b4e234b90121
Jason Lengstorf <jason@lengstorf.com>
2020-03-10 10:46:55 -0700

    feat: refactor to use new components

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useMachine } from '@xstate/react';
 import dashboardMachine from '../state-machines/dashboard';
 import Layout from '../components/layout';
 import Login from '../components/login';
 import LoggingIn from '../components/logging-in';
 import Overlay from '../components/overlay';
+import LoadingSites from '../components/loading-sites';
+import SiteChooser from '../components/site-chooser';
 import EffectsList from '../components/effects-list';
 
 export default () => {
-  const [siteID, setSiteID] = useState();
   const [state, send] = useMachine(dashboardMachine);
 
   return (
@@ -17,30 +18,10 @@ export default () => {
 
       {state.matches('login') && <LoggingIn />}
 
+      {state.matches({ configure: 'loading' }) && <LoadingSites />}
+
       {state.matches({ configure: 'setSiteID' }) && (
-        <form
-          onSubmit={event => {
-            event.preventDefault();
-            send('SET_SITE_ID', { site: siteID });
-          }}
-        >
-          <label htmlFor="netlifySiteID">
-            Which site has your effects functions?
-          </label>
-          <select
-            name="netlifySiteID"
-            id="netlifySiteID"
-            onBlur={event => setSiteID(event.target.value)}
-          >
-            <option value="">-- choose a site from the dropdown --</option>
-            {state.context.sites.map(({ id, url }) => (
-              <option key={id} value={id}>
-                {url}
-              </option>
-            ))}
-          </select>
-          <button type="submit">Save Site ID</button>
-        </form>
+        <SiteChooser state={state} send={send} />
       )}
 
       {state.matches({ display: 'loaded' }) && (
